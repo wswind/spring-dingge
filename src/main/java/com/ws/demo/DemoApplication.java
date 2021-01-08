a7834f5a76c5ba4276fec76b617b503f3b34b4d5
ws <wswind@foxmail.com>
2021-01-08 15:46:28 +0800

    l03

---
+++ src/main/java/com/ws/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.ws.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args->{
+			System.out.println("Inspect beans by spring boot:");
+			var beanNames = ctx.getBeanDefinitionNames();
+			
+		};
+	}
+
+}
