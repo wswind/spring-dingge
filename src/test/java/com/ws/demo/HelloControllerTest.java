92333c21b610c552655cba66cd6420a5294e9aa4
ws <wswind@foxmail.com>
2021-01-08 16:39:55 +0800

    l04 finish

---
+++ src/test/java/com/ws/demo/HelloControllerTest.java
@@ -0,0 +1,26 @@
+package com.ws.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getHello() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/")
+        .accept(MediaType.APPLICATION_JSON))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.content().string(equalTo("Hello World!")));
+    }
+}
